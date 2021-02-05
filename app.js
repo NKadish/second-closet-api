79d4916a81eebd6e06a7351b7dd9f27eec8e0862
NKadish <noahkadish@hotmail.com>
2021-02-05 22:44:26 +0000

    Added quoteHelpers for genericQuote and customerQuote taking into account the different kinds of discount

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const customersRouter = require('./routes/index');
+const flatFeeRouter = require('./routes/users');
+const pricingRouter = require('./routes/pricing');
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+const db = require('./db');
+const dbHelpers = require('./helpers/dbHelpers')(db);
+
+app.use('/customers', customersRouter(dbHelpers));
+app.use('/flatfee', flatFeeRouter(dbHelpers));
+app.use('/pricing', pricingRouter(dbHelpers));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ helperFunctions/quoteHelper.js
@@ -0,0 +1,73 @@
+module.exports = () => {
+
+  const genericQuote = (items, flatFee) => {
+      let quote = items.length * flatFee;
+      return quote;
+      // fix pricing so that it includes $ + .00
+  };
+
+  const customerQuote = (items, flatFee, customer) => {
+
+    const originalQuote = items.length * flatFee;
+    let updatedQuote = originalQuote
+
+    if (customer.per_unit_of_volume) {
+      let volume = 0;
+      items.forEach(element => {
+        let itemsVolume = element.length * element.width * element.height
+        volume += itemsVolume;
+      });
+      updatedQuote += customer.per_unit_of_volume * volume;
+    };
+
+    if (customer.percent_of_value_charge) {
+      let value = 0;
+      items.forEach(element => {
+        value += element.value;
+      });
+      updatedQuote += value * .05; 
+    };
+
+    if (customer.first_hundred_discount) {
+      updatedQuote -= (customer.first_hundred_discount / 100) * updatedQuote
+    }
+
+    // problem here though, what about the volume cost? 
+    if (customer.second_hundred_discount && items.length > 100) {
+      let quotePastHundred = 0
+      for (let i = 101; i < items.length; i++) {
+        if (customer.per_unit_of_volume) {
+          quotePastHundred += (element.length * element.width * element.height) * customer.per_unit_of_volume
+        };
+        quotePastHundred += flatFee;
+      }
+      updatedQuote -= quotePastHundred;
+      updatedQuote += quotePastHundred * (customer.second_hundred_discount / 100);
+    }
+
+    if (customer.discount_after && items.length > 200) {
+      let quotePastTwohundred = 0
+      for (let i = 201; i < items.length; i++) {
+        if (customer.per_unit_of_volume) {
+          quotePastHundred += (element.length * element.width * element.height) * customer.per_unit_of_volume
+        };
+        quotePastTwohundred += flatFee;
+      }
+      updatedQuote -= quotePastTwohundred;
+      updatedQuote += quotePastTwohundred * (customer.discount_after / 100);
+    }
+
+    if (customer.discount) {
+      updatedQuote -= (customer.discount / 100) * updatedQuote;
+    };
+
+    // fix pricing so that it includes $ + .00
+    return updatedQuote;
+
+  };
+
+  return {
+    genericQuote,
+    customerQuote
+  };
+};
